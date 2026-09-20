5b24f2b064fc4807f7ab64e4e1262f20136e2d61
via <ch4rli.33x@gmail.com>
2026-09-20 04:18:57 +0000

    Try fixing this error: `Runtime ReferenceError: Users is not defined. Er

--- src/app/channel/my-channel/page.tsx
+++ src/app/channel/my-channel/page.tsx
@@ -5,7 +5,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 import { useUser, useDoc, useMemoFirebase, useFirestore, useCollection } from "@/firebase"
 import { doc, setDoc, collection, query, where, getDoc } from "firebase/firestore"
-import { X, Check, MessageSquare, Smile, Home, BadgeCheck, Loader2 } from "lucide-react"
+import { X, Check, MessageSquare, Smile, Home, BadgeCheck, Loader2, Users } from "lucide-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 
